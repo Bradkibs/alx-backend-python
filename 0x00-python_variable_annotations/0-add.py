b82a9640737fc8459b5a673419c38e801b15d228
Bradleykibs <Bradleykibs@gmail.com>
2023-04-27 13:17:56 +0300

    the first task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Writing a type-annotated add function"""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns the sum"""
+    return a + b
