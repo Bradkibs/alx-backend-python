cbec2ceea602e3872ad519133b874c3c12450fef
Bradleykibs <Bradleykibs@gmail.com>
2023-04-27 13:47:43 +0300

    to str implementation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A  function that takes a float and returns
+string representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """Returns n as a string representation"""
+    return str(n)
