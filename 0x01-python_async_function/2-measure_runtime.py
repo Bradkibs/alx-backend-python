9f1f8a70462294a99264b73a596e87260592bff5
Bradleykibs <Bradleykibs@gmail.com>
2023-05-05 12:38:54 +0300

    measure runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A function that meatures total execution time"""
+
+
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """returns total_time / n"""
+
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.time() - start
+    return total_time / n
