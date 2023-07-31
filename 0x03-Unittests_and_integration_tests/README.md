8fbcc1d29d3d604a57732c93e682161f6ee6d3a2
Bradkibs <Bradleykibs@gmail.com>
2023-07-31 15:45:45 +0300

    testing access nested map

---
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -0,0 +1,6 @@
+# Learning about Integration tests
+* [Unittest python framework](https://docs.python.org/3/library/unittest.html)
+* [Unittest.mock](https://docs.python.org/3/library/unittest.html)
+* [How to mock readonly property with mock](https://stackoverflow.com/questions/11836436/how-to-mock-a-readonly-property-with-mock)
+* [parameterized](https://pypi.org/project/parameterized/)
+* [Memoization](https://en.wikipedia.org/wiki/Memoization)
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env pythond3
+"""Unittest for access_nested_map function of utils module"""
+
+
+from utils import access_nested_map
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """A unittest for the access_nested_map function"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ])
+    def test_access_access_nested_map(self, nested_map, path, answer):
+        """testing function"""
+        self.assertEqual(access_nested_map(nested_map, path), answer)
